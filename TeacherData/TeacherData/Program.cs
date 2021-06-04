ec1c5ca25333255cb97a2b5c8382c3cd3b4d44dd
sramohale0@outlook.com <sramohale0@outlook.com>
2021-06-04 11:51:59 +0200

    first commit

---
+++ TeacherData/TeacherData/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace TeacherData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter ID: ");
+            //ENTER Input
+            string T_IDNumber = Console.ReadLine();
+            Console.WriteLine("Enter Name: ");
+            //ENTER Input
+            string T_Name = Console.ReadLine();
+            Console.WriteLine("Enter Class and Section: ");
+            //ENTER Input
+            string T_Class = Console.ReadLine();
+
+
+            string fileName = @"C:\Users\KABELO\Documents\LocalRepo\Phace1_project\Teacher.txt";
+
+            try
+            {
+
+                // Check if file already exists. If yes, delete it.    
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a new file    
+                using (StreamWriter fs = new StreamWriter(fileName)) 
+                {
+                    // Add some text to file    
+                    fs.WriteLine("The ID of the teacher is: " + T_IDNumber);
+                    fs.WriteLine("The name of the teacher is: " + T_Name);
+                    fs.WriteLine("The class and section of the teacher is: " + T_Class);                 
+                }
+                // Open the stream and read it back.    
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+
+
+           
+        }
+    }
+    
+}
